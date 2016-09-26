3ed2195afa4825c3115750e6de24cca9785aeff5
Chen Ningshuang <cnsapril@gmail.com>
2016-09-27 02:43:09 +0800

    建立了牌堆，手牌，游戏，玩家和选择地主的方式。

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 
-from random import shuffle
+from random import shuffle, randint
+
+suit_symbol = [u'\u2663', u'\u2666', u'\u2665', u'\u2660', u"\U0001F0BF", u"\U0001F0CF"]
 
 
 class Suits(object):
@@ -12,26 +14,47 @@ class Suits(object):
     b_joker = 4
     s_joker = 5
 
-suit_symbol = [u'\u2663', u'\u2666', u'\u2665', u'\u2660']
-
 
 class Card(object):
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
 
-    def __repr__(self):
-        return suit_symbol[self.suit] + str(self.value)
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __cmp__(self, other):
+        return self.value - other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __unicode__(self):
+        if self.value <= 10:
+            temp_value = str(self.value)
+        else:
+            switcher = {
+                11: u"J",
+                12: u"Q",
+                13: u"K",
+                14: u"A",
+                15: u"2",
+                16: u"小王",
+                17: u"大王",
+            }
+            temp_value = switcher.get(self.value)
+
+        return suit_symbol[self.suit] + temp_value
 
 
 class Deck(object):
     def __init__(self):
         self.cards = []
         for suit in [Suits.clubs, Suits.diamonds, Suits.hearts, Suits.spades]:
-            for value in xrange(1, 14):
+            for value in xrange(3, 16):
                 self.cards.append(Card(value, suit))
-        self.cards.append(Card(14, Suits.s_joker))
-        self.cards.append(Card(15, Suits.b_joker))
+        self.cards.append(Card(16, Suits.s_joker))
+        self.cards.append(Card(17, Suits.b_joker))
         shuffle(self.cards)
 
     def draw(self):
@@ -50,18 +73,83 @@ class Hand(object):
             self.cards.append(deck.draw())
 
     def take_bottom(self, deck):
+        print "底牌：",
         for i in xrange(3):
-            self.cards.append(deck.draw())
+            temp_card = deck.draw()
+            print temp_card,
+            self.cards.append(temp_card)
+        print
 
         if not deck.is_empty():
             print "Error assigning cards. Card is not empty after the operation! Exiting..."
             quit()
 
     def print_hand(self):
+        self.cards.sort(reverse=True)
         print "你的手牌:",
-        for card in self.cards:
-            print card
+        for current_card in self.cards:
+            print current_card,
+        print
 
 
-card = Card(7, Suits.clubs)
-print card
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.is_winner = False
+        self.hand = None
+
+    def __str__(self):
+        return unicode(self).encode("utf-8")
+
+    def __unicode__(self):
+        return self.name
+
+    def be_landlord(self, deck):
+        self.hand.take_bottom(deck)
+
+
+class Game(object):
+    def __init__(self):
+        self.players = []
+        self.deck = Deck()
+        self.landlord = None
+
+    def join(self, player):
+        if len(self.players) > 3:
+            print "本场游戏人数已满，请重新开始一轮新的游戏"
+        else:
+            self.players.append(player)
+            print "玩家" + player.name + "加入游戏"
+
+    def start(self):
+        if len(self.players) < 3:
+            print "斗地主需要3人才可进行，目前人数不足，游戏不能开始"
+        else:
+            landlord_pos = randint(0, 2)
+            self.landlord = self.players[landlord_pos]
+            for player in self.players:
+                player.hand = Hand(self.deck)
+                print "玩家" + player.name,
+                player.hand.print_hand()
+            current_pos = landlord_pos
+            for i in range(3):
+                current_pos = (current_pos + 1) % 3
+                if raw_input("玩家" + self.players[current_pos].name + "：是否抢地主？") == 'y':
+                    self.players[current_pos].be_landlord(self.deck)
+                    print "本轮地主：" + self.landlord.name
+                    print "地主" + self.landlord.name,
+                    self.landlord.hand.print_hand()
+                    break
+            else:
+                print "没有玩家选择当地主，游戏结束。请开始新一轮游戏。"
+
+
+game = Game()
+cqc = Player("陈倩偲")
+cns = Player("陈凝霜")
+szy = Player("石真玉")
+game.join(cqc)
+game.join(cns)
+game.join(szy)
+game.start()
+
